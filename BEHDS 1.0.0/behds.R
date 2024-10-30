a5db2d921c1a1abe87bde38b6af1be804090c389
FloPaho <florian.pahovnikar@gmail.com>
2024-10-30 10:25:43 +0100

    Delete BEHDS 1.0.0 directory

--- BEHDS 1.0.0/behds.R
+++
@@ -1,7 +0,0 @@
-#enter the Project name                                                                                    Script by Florian Pahovnikar
-proj="Example"
-
-path=dirname(rstudioapi::getSourceEditorContext()$path)
-setwd(file.path(path,"code"))
-source("Code.R")
-
